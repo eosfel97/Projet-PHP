37dde922b4db714bc16606fe9a970406774f0305
eosfel97 <any.gouba@gmail.com>
2022-01-03 14:05:35 +0100

    aaaaaaaa

--- EditUser.php
+++ EditUser.php
@@ -64,10 +64,6 @@ $counter = 0;
   <button type="submit" class="btn btn-primary bg-danger mt-5 ms-5">Modifer les informations</button>
 </form>
       </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
     </div>
   </div>
 </div>
